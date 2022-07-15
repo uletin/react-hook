7290d16dbcf923d3f07de30abc2b28feb039cc36
uletin <dokterkoding111@gmail.com>
2022-07-15 14:39:59 +0800

    belajar react hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import UseEffect from './components/UseEffect'
 
 function App() {
+  const [toogle, setToogle] = useState(true)
+  const handleToogle = () => setToogle(currentState => !currentState)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleToogle}>Toogle</button>
+      {toogle && <UseEffect />}
     </div>
   );
 }
---
+++ src/components/UseEffect.js
@@ -0,0 +1,55 @@
+import userEvent from "@testing-library/user-event";
+import React, {useState, useEffect} from "react";
+
+function MyFunction(){
+    console.log('-> Function Init')
+
+    const [count, setCount] = useState(0)
+    const handleCount = () => {
+        setCount(prevState => {
+            return prevState+1
+        })
+    }
+
+    //LifeCycle
+
+    // run on every render
+    useEffect(() => {
+        console.log('my first effect')
+    })
+
+    //componentDidMount
+    useEffect(() => {
+        console.log('ComponentDidMount equivalent')
+    }, [])
+
+    //componenDidUpdate
+    useEffect(() => {
+        if (count > 0) {
+            console.log('ComponentDidUpdate equivalent')
+        } 
+    }, [count])
+
+    // ComponentWillUnMount
+    useEffect(() => {
+        // clean up function
+        return(
+            () => {
+                console.log("ComponentWillUnMount equivalent")
+            }
+        ) 
+    }, [])
+
+    console.log(`-> Start Render (${count})`)
+    return(
+        <div>
+                <h1>Function Component</h1>
+                <p>
+                    <button onClick={handleCount}>Count</button>
+                    {count}
+                </p>
+        </div>
+    )
+}
+
+export default MyFunction
